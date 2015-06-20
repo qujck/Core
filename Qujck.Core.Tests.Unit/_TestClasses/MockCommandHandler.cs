3a987168c2d6b6abbed65755a3e61aab965b7ebd
Peter Parker <social@qujck.com>
2015-06-20 10:16:09 +0100

    New tests Command Handler Event Decorator

---
+++ Qujck.Core.Tests.Unit/_TestClasses/MockCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Qujck.Core.Commands;
+
+namespace Qujck.Core.Tests.Unit
+{
+    internal class MockCommandHandler<TCommand> : 
+        ICommandHandler<TCommand> where TCommand : ICommand
+    {
+        public TCommand Command;
+        public Action<TCommand> Action;
+
+        public MockCommandHandler() { }
+
+        public MockCommandHandler(Action<TCommand> action)
+        {
+            this.Action = action;
+        }
+
+        public void Handle(TCommand command)
+        {
+            this.Command = command;
+            if (this.Action != null)
+            {
+                this.Action(command);
+            }
+        }
+    }
+}
---
+++ Qujck.Core.Tests.Unit/_TestClasses/StubQueryHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Qujck.Core.Queries;
+
+namespace Qujck.Core.Tests.Unit
+{
+    internal class StubQueryHandler<TQuery, TResult> : 
+        IQueryHandler<TQuery, TResult> where TQuery : IQuery<TResult>
+    {
+        public TQuery Query;
+        public TResult Result;
+        public Action Action;
+
+        public StubQueryHandler() { }
+
+        public StubQueryHandler(TResult result)
+        {
+            this.Result = result;
+        }
+
+        public StubQueryHandler(Action action, TResult result)
+        {
+            this.Action = action;
+            this.Result = result;
+        }
+
+        public StubQueryHandler(Action action)
+        {
+            this.Action = action;
+        }
+
+        public TResult Handle(TQuery query)
+        {
+            this.Query = query;
+            if (this.Action != null)
+            {
+                this.Action();
+            }
+            return this.Result;
+        }
+    }
+}
